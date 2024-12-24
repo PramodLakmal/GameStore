0a2598df7e660206038e0f6f7a3c0c24d53b1fbd
Pramod Lakmal <pramodlakmalofficial@gmail.com>
2024-12-24 15:23:16 +0530

    Add initial game data and configure API settings and implement dtos

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,5 +1,39 @@
+using GameStore.Api.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+List<GameDto> games = [
+    new (
+        1,
+        "The Last of Us Part II",
+        "Action-adventure",
+        59.99m,
+        new DateOnly(2020, 6, 19)
+    ),
+    new (
+        2,
+        "Cyberpunk 2077",
+        "Action role-playing",
+        59.99m,
+        new DateOnly(2020, 12, 10)
+    ),
+    new (
+        3,
+        "Death Stranding",
+        "Action",
+        59.99m,
+        new DateOnly(2019, 11, 8)
+    ),
+    new (
+        4,
+        "The Witcher 3: Wild Hunt",
+        "Action role-playing",
+        39.99m,
+        new DateOnly(2015, 5, 19)
+    )
+
+];
 
 app.MapGet("/", () => "Hello World!");
 
