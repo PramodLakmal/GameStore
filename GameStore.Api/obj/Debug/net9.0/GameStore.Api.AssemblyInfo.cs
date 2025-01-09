31f05d1c66f1900fcac76b853463b33865e9559a
Pramod Lakmal <pramodlakmalofficial@gmail.com>
2025-01-09 23:51:20 +0530

    Saving new entities to the database

--- GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca6d1315aae1c42d192e6e8c35cc80998fa9b6bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52e9344b95941950a3eb794a9f1c6313bd0e7648")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
