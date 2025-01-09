52e9344b95941950a3eb794a9f1c6313bd0e7648
Pramod Lakmal <pramodlakmalofficial@gmail.com>
2025-01-09 23:24:25 +0530

    Seed data

--- GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d20d29ad9a4cafb8ec91eb7358e894689d5ea52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca6d1315aae1c42d192e6e8c35cc80998fa9b6bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
