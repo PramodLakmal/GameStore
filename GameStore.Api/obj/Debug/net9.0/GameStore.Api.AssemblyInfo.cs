24b04f568b3c8c5c4874b1b7104c63ead80fa9e7
Pramod Lakmal <pramodlakmalofficial@gmail.com>
2025-01-10 00:01:48 +0530

    Mapping entities to DTOs

--- GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52e9344b95941950a3eb794a9f1c6313bd0e7648")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31f05d1c66f1900fcac76b853463b33865e9559a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
