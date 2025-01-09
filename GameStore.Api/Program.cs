0d20d29ad9a4cafb8ec91eb7358e894689d5ea52
Pramod Lakmal <pramodlakmalofficial@gmail.com>
2025-01-09 21:32:41 +0530

    Use the ASP.NET Core configuration system

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -3,7 +3,7 @@ using GameStore.Api.EndPoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = "Data Source=gamestore.db";
+var connectionString = builder.Configuration.GetConnectionString("GameStore");
 builder.Services.AddSqlite<GameStoreContext>(connectionString);
 
 var app = builder.Build();
